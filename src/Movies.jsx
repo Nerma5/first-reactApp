ef7e5f4bfd301b0bbe29be8706de7f18d614c205
Nerma5 <nermahh5@gmail.com>
2023-03-27 18:35:54 +0200

    refactored code

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,7 +1,18 @@
 import React, {useState, useEffect} from "react";
 
 const Movies = () =>{
-
+useEffect(()=>{
+    fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=game%20of%20thr',{
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'bca2c254demsh6a0091815b9b001p1719fejsn3f06a878686c',
+            'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+        }
+    },[])
+    .then(res => console.log(res.json()))
+    .catch(err => console.log(err))
+})
+    
     return(
         <div className="movies">
 
