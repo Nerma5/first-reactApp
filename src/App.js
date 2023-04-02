9658cc52a2718098714f9c3d39227dc7a7c2c44b
Nerma5 <nermahh5@gmail.com>
2023-04-02 15:54:37 +0200

    added Movies

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import {Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Header from "./pages/header";
-
+import Movies from './Movies'
 
 function App() {
 
   return (
     <> 
-  <Header />
+  {/* <Header />
     <Routes>
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/about" element={<About/>}/>/
-    </Routes>
+    </Routes> */}
+    <Movies />
     </>
   );
 }
