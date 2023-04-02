f21b38a9e681eb6de9e597f0c064bf01e63ef77f
Nerma5 <nermahh5@gmail.com>
2023-04-02 23:03:51 +0200

    practicing fetching data

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -1,7 +1,13 @@
 import React,{useState, useEffect} from "react";
 
 const FetchData = () =>{
-
+const[post, SetPost] = useState([])
+useEffect(
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {SetPost(data)})
+    .catch(err => console.log(err))
+)
 
     
     return(
