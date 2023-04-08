3c1008fba9777fd4ced20a63662bd0cae4d48456
Nerma5 <nermahh5@gmail.com>
2023-04-08 22:41:12 +0200

    pracitce

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ const [name, setName] = useState("nerma")
   return (
     <div className="App">
       {/* <h1>Hello react</h1> */}
-      {/* <CreatTweet />
-      <TweetList name={name} /> */}
+
       <TweetList />
       <FetchData />
     
