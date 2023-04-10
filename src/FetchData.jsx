93a3011cd884c589a79678eef0c0748beee63e60
Nerma5 <nermahh5@gmail.com>
2023-04-10 22:58:28 +0200

    pracite

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -1,13 +1,7 @@
 import React,{useState, useEffect} from "react";
 
 const FetchData = () =>{
-    const[user, setUser] = useState([])
-    useEffect(()=>{
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(data => {setUser(data)})
-        .catch(err => console.log(err))
-    }, [])
+
 
     
     return(
