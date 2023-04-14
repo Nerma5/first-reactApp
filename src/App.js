bc2ca0413393b1da38d0f8e54272d8488675aca0
Nerma5 <nermahh5@gmail.com>
2023-04-14 04:50:32 +0200

    init states

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Tweet from "./components/Tweet";
+// import Tweet from "./components/Tweet";
 import CreatTweet from "./components/CreatTweet";
 import TweetList from "./components/TweetList";
+import React, {useState} from "react";
+//using states 06:37
 
 
 function App() {
+  const sayHello = (user) =>{
+    console.log(`i just wanted to say hello to ${user}`) //events
+  }
   return (
     <div className="App">
       <h1>Hello react</h1>
-      <CreatTweet />
-      <TweetList />
+      {/* <CreatTweet />
+      <TweetList /> */}
+      <button onClick={()=> sayHello("Nerma")}>Click</button>
     </div>
   );
 }
