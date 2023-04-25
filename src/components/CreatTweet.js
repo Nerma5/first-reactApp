8cd192e133b4aa90c278ee09b3ec80e9f3d32658
Nerma5 <nermahh5@gmail.com>
2023-04-25 20:43:47 +0200

    useStates

--- src/components/CreatTweet.js
+++ src/components/CreatTweet.js
@@ -10,6 +10,7 @@ const CreatTweet = () =>{
         <form>
             <textarea value={textInput} onChange={userInputHandler} cols={50} rows={5} ></textarea>
             <button >Submit</button>
+            <h1  onClick={()=> setTextInput("")}>{textInput}</h1>
         </form>
     )
 
