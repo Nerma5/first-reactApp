a21eeed288e1b1225ba84092ae4d0207c803f683
Nerma5 <nermahh5@gmail.com>
2023-04-26 15:50:56 +0200

    practice

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -2,7 +2,14 @@ import React,{useState, useEffect} from "react";
 
 
 const Practice = () =>{
+const[types, setTypes] = useState([])
 
+useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/comments?postId=1")
+    .then(res => res.json())
+    .then(data => {setTypes(data)})
+    .catch(err => console.log(err))
+},[])
     return(
         <div className="practice">
 
