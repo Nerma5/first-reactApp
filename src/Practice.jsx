af13a5bf2dd234569469973d64fbf1990cf0d5a7
Nerma5 <nermahh5@gmail.com>
2023-05-11 15:45:52 +0200

    practice

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -3,24 +3,9 @@ import React,{useState, useEffect} from "react";
 
 const Practice = () =>{
 
-    const[comments, setComments] = useState([])
-
-    useEffect( () =>{
-        fetch("https://jsonplaceholder.typicode.com/comments")
-        .then(res => res.json())
-        .then(data => {setComments(data)})
-        .catch(err => console.log(err))
-    },[])
-
     return(
         <div className="practice">
-                {comments.length > 0 && (
-                    <ul>
-                        {comments.map(comment =>(
-                            <li key={comment.id}>{comment.name}</li>
-                        ))}
-                    </ul>
-                )}
+
         </div>
     )
 }
