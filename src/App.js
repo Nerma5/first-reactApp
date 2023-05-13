92a139bda2da309dbb9aa736daae3ca8001ae213
Nerma5 <nermahh5@gmail.com>
2023-05-13 20:48:29 +0200

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@
 // import TweetList from "./components/TweetList";
 import React, {useState} from "react";
 //using states 06:37
-import FetchData from "./FetchData";
 
 function App() {
 const [name, setName] = useState("nerma")
@@ -14,7 +13,6 @@ const [name, setName] = useState("nerma")
       {/* <CreatTweet />
       <TweetList name={name} />
       <TweetList /> */}
-      <FetchData />
       {/* <button>Click</button> */}
     </div>
   );
