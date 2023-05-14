93b1d49ad1da44e59ae7640628e43f33e4bf6f59
Nerma5 <nermahh5@gmail.com>
2023-05-14 23:13:21 +0200

    practice

--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -2,16 +2,22 @@ import React,{useState, useEffect} from "react";
 
 const FetchData = () =>{
 const[post, SetPost] = useState([])
-useEffect(
-    fetch("https://jsonplaceholder.typicode.com/posts")
+useEffect(()=>{
+     fetch("https://jsonplaceholder.typicode.com/posts")
     .then(res => res.json())
     .then(data => {SetPost(data)})
     .catch(err => console.log(err))
-)
+},[])
 
-    
     return(
         <div className="fetch">
+            {post.length > 0 && (
+                <ul >
+                    {post.map(post =>(
+                        <li key={post.id}>{post.name}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 }
