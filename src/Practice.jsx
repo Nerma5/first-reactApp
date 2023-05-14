a6d239770b667f8d1de8a9488427c6d172d9aa07
Nerma5 <nermahh5@gmail.com>
2023-05-15 00:53:21 +0200

    practice

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -2,10 +2,26 @@ import React, {useState, useEffect} from "react";
 
 const Practice = () =>{
 
+const[photos, setPhotos]= useState([])
+
+useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/photos")
+    .then(res => res.json())
+    .then(data => {setPhotos(data)})
+    .catch(err => console.log(err))
+},[])
 
 
     return(
         <div className="practice">
+            {photos.length > 0 && (
+                <ul>
+                    {photos.map(photo =>(
+                        <li key={photo.id}>{photo.url}</li>
+                    ))}
+                </ul>
+            )}
+
         </div>
     )
 }
