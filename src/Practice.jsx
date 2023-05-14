104b7446be04c73b46c4195dfb2fdbb14c0e8d71
Nerma5 <nermahh5@gmail.com>
2023-05-15 00:39:15 +0200

    blank

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -2,26 +2,10 @@ import React, {useState, useEffect} from "react";
 
 const Practice = () =>{
 
-const[toDo, setToDo] = useState([])
 
-useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(res => res.json())
-    .then(data => {setToDo(data)})
-    .catch(err => console.log(err))
-},[])
 
     return(
         <div className="practice">
-            <h1>Hello world</h1>
-            <h2>Here is some of toDos:</h2>
-            {toDo.length > 0 && (
-                <ul>
-                    {toDo.map(toDo =>(
-                        <li key={toDo.id}>{toDo.title}</li>
-                    ))}
-                </ul>
-            )}
         </div>
     )
 }
