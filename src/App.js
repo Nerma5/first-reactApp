dcc0be17417bb8bb077faa4f3b0b31c9c03b3066
Nerma5 <nermahh5@gmail.com>
2023-05-16 19:49:02 +0200

    added new file for class

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import React, {useState} from "react";
 import FetchData from "./FetchData";
 import Practice from "./Practice";
 import Movies from './Movies'
+import Project from "./Project";
+
 function App() {
 
   return (
     <div className="App">
-    <Movies />
+    {/* <Movies /> */}
+    <Project />
     </div>
   );
 }
---
+++ src/Project.jsx
@@ -0,0 +1,23 @@
+import React,{useState} from "react";
+
+
+const Project = () =>{
+
+    const[drop, setDrop] = useState('')
+
+    const[textInput, setTextInput] = useState("")
+    const userInputHandler = (e) =>{
+        setTextInput(e.target.value)
+    }
+    return(
+        <div>
+             <form>
+            <input value={textInput} onChange={userInputHandler} cols={50} rows={5} />
+            <button >Submit</button>
+            <h1    onClick={()=> setTextInput("")}>Text:{textInput}</h1>
+        </form>
+        </div>
+    )
+}
+
+export default Project
