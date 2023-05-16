ac57ec0437d336faa1c3d125e226bc3da452af0e
Nerma5 <nermahh5@gmail.com>
2023-05-16 20:20:42 +0200

    dropdown

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -8,20 +8,18 @@ const dropHandler = (e) =>{
     setDrop(e.target.value)
 }
 
-
     return(
         <div>
-        <label value={drop} onChange={dropHandler}>Choose a color:</label> 
-    <select> 
+        <label>Choose a color:</label> 
+    <select value={drop}  onChange={dropHandler}> 
         <option>red</option> 
         <option>yellow</option> 
         <option>green</option> 
         <option>orange</option> 
     </select>
-    <p>You picked:{setDrop} color</p>
+    <h1>Color you picked:{drop}</h1>
         </div>
     )
 }
-
 
 export default Dropdown
