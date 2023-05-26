8f4d256d7dab6ff4dcd31b1b59b96edb3a21dfcf
Nerma5 <nermahh5@gmail.com>
2023-05-26 20:11:30 +0200

    added carousel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Header from "./pages/header";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import Movies from './Movies'
 
 function App() {
