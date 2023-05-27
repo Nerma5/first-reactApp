3ecec4f5d000cacb7f8419d022e73d4f48369155
Nerma5 <nermahh5@gmail.com>
2023-05-27 14:48:17 +0200

    terenary operators

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -8,7 +8,11 @@ const Header = () =>{
         <div className="header">
             <h4>My new page</h4>
                 <ul>
-                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink to='/' style={({ isActive, isPending }) => {
+                    return {
+                            fontWeight: isActive ? "bold" : "",
+                            color: isPending ? "red" : "black",
+                                                } } }>Home</NavLink></li>
                     <li><NavLink to= '/about'>About</NavLink></li>
                     <li><NavLink to='/contact-us'>Contact</NavLink></li>
                     <li><NavLink to='/join-us'>Join us</NavLink></li>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,8 @@ const Home = () =>{
     return(
         <div className="home">
             <h1>Home</h1>
+            <p>Welcome to the home page</p><br/> in next section you are going to learn a lot about us
             </div>
-        
     )
 }
 
