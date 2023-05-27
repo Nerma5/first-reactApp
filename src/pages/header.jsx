38b856350de296d5fc36ff933a5a6dd679695471
Nerma5 <nermahh5@gmail.com>
2023-05-27 19:51:54 +0200

    terenary operators

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -13,9 +13,21 @@ const Header = () =>{
                             fontWeight: isActive ? "bold" : "",
                             color: isPending ? "red" : "black",
                                                             } } }>Home</NavLink></li>
-                    <li><NavLink to= '/about'>About</NavLink></li>
-                    <li><NavLink to='/contact-us'>Contact</NavLink></li>
-                    <li><NavLink to='/join-us'>Join us</NavLink></li>
+                    <li><NavLink to= '/about' style={({ isActive, isPending }) => {
+                    return {
+                            fontWeight: isActive ? "bold" : "",
+                            color: isPending ? "red" : "black",
+                                                            } } }>About</NavLink></li>
+                    <li><NavLink to='/contact-us' style={({ isActive, isPending }) => {
+                    return {
+                            fontWeight: isActive ? "bold" : "",
+                            color: isPending ? "red" : "black",
+                                                            } } }>Contact</NavLink></li>
+                    <li><NavLink to='/join-us' style={({ isActive, isPending }) => {
+                    return {
+                            fontWeight: isActive ? "bold" : "",
+                            color: isPending ? "red" : "black",
+                                                            } } }>Join us</NavLink></li>
                 </ul>
         </div>
     )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ const Home = () =>{
     return(
         <div className="home">
             <h1>Home</h1>
-            <p>Welcome to the home page</p><br/> in next section you are going to learn a lot about us
+            <p>Welcome to the home page</p><br/> in next section you are going to learn a lot of about us
             </div>
     )
 }
