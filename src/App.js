5b21e82dd31e36c6287d1794012d4ad5d9be04e8
Nerma5 <nermahh5@gmail.com>
2023-06-30 20:00:44 +0200

    creacted to do list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ToDoList from "./api";
 
 
 function App() {
   return (
     <>
+    <ToDoList/>
     </>
   );
 }
---
+++ src/api/index.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { createTodo, fetchTodos } from "./api";
+//CRUD
+
+const ToDoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await fetchTodos();
+  //     setTodos(data);
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  const handleCreateTodo = async () => {
+    const newTodo = { title: newItem };
+    const createdTodo = await createTodo(newTodo);
+    setTodos([...todos, createdTodo]);
+  };
+
+  return (
+    <div>
+      <input
+        placeholder="New to do"
+        onChange={(e) => setNewItem(e.target.value)}
+      ></input>
+      <button onClick={handleCreateTodo}>Add toDo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
