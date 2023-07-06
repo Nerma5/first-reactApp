ba55e9e90b3f135ad336d240dd72453cc6246f85
Nerma5 <nermahh5@gmail.com>
2023-07-06 20:27:43 +0200

    carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ToDoList from "./api";
+import Carousel from "./practice/carousel practice/Carousel";
 // import styled from 'styled-components
 
 function App() {
   return (
     <>
-      <ToDoList />
+      {/* <ToDoList /> */}
+      <Carousel/>
     </>
   );
 }
